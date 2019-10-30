2fe93686976ba30911e4c95791b8ee07ff0e33e7
Dmytro Zemnytskyi (pragmasoft) <pragmasoft@gmail.com>
2019-10-30 16:28:14 +0200

    imported project template

---
+++ src/test/java/ua/pragmasoft/jlinkplayground/JlinkPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.pragmasoft.jlinkplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JlinkPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
